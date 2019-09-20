50b7a38c7a8f20ba8005f870b795647558cf8a07
Serge Smirnov <sms-system@shri2018.yaconnect.com>
2019-09-20 20:19:26 +0300

    Implemented basic then behavior

--- index.js
+++ index.js
@@ -8,10 +8,40 @@ var PPromise = (function () {
     if (typeof fn !== 'function') {
       throw new Error('Promise argument must be a function')
     }
-  }
 
-  Promise.prototype.then = function (fn) {
-    console.log('THEN')
+    var wasResolved = false, wasRejected = false
+    var resolveHandler, rejecteHandler
+    var resolvedValue
+
+    this.then = function (resolve, reject) {
+      if (typeof resolve !== 'function') {
+        resolve = function () { return resolvedValue }
+      }
+
+      resolveHandler = resolve
+      rejecteHandler = reject
+
+      if (wasResolved) {
+        return resolve(resolvedValue)
+      }
+    }
+
+    var resolve = function (result) {
+      if (wasResolved || wasRejected) { return }
+      wasResolved = true
+      if (resolveHandler) { resolveHandler(result) }
+    }
+
+    var reject = function () {
+      if (wasResolved || wasRejected) { return }
+      wasRejected = true
+    }
+
+    try {
+      fn(resolve, reject)
+    } catch (err) {
+      reject(err)
+    }
   }
 
   return Promise
